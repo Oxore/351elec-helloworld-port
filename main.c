64085b5922cd005ca3747073107bda8f2668f4d0
Oxore <oxore@protonmail.com>
2021-08-07 16:09:06 +0300

    Tinker with inputs a little

--- main.c
+++ main.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_ttf.h>
+#include <inttypes.h>
+
+// SDL_JOYBUTTONDOWN(0x603) and SDL_JOYBUTTONUP(0x604).
+// - Created when
+//   - A, B, X, Y
+//   - Start, Select,
+//   - L1, R1, L2, R2, L3, R3 (yep, stick presses are here)
+// - Button ID may be acquired from jbutton.button.
+//
+// SDL_JOYHATMOTION(0x602) created when D-pad pressed
+// - Button ID may be acquired from jhat->value.
+// - Button ID is basically just a bit and if every button is pressed then all
+//   bits are set. Bits are SDL_HAT_(UP|DOWN|RIGHT|LEFT) from SDL_joystick.h.
+//
+// SDL_JOYAXISMOTION(0x600) created when L3 or R3 stick tilted.
+// - Tilt value is int16 and may be acquired from jaxis.value.
+// - Axis ID may be acquired from jaxis.axis.
 
 #define SCREEN_WIDTH 640
 #define SCREEN_HEIGHT 480
@@ -29,16 +46,48 @@ Uint32 exit_timer_cb(Uint32 interval, void *param)
     event.user = userevent;
 
     SDL_PushEvent(&event);
-    return(interval);
+    return interval;
 }
 
-void bake_message(
+void bake_event(
         SDL_Renderer* renderer,
         TTF_Font* font,
         SDL_Surface** surface,
         SDL_Texture** texture,
-        const char* text)
+        const SDL_Event* event)
 {
+    char text[100] = {0};
+    int size = snprintf(text, sizeof(text), "type=0x%" PRIX32, event->type);
+            //event->jhat.value);
+            //"jhat->value=0x%" PRIX32 ",\n",
+            //"jaxis->value=0x%" PRIX32 ",\n",
+
+    if (size >= 0 && (size_t)size <= sizeof(text))
+    {
+        if (event->type == SDL_JOYHATMOTION)
+        {
+            snprintf(
+                    text + size,
+                    sizeof(text) - size,
+                    " d-pad button=0x%" PRIX8, event->jhat.value);
+        }
+        else if (event->type == SDL_JOYAXISMOTION)
+        {
+            snprintf(
+                    text + size,
+                    sizeof(text) - size,
+                    " axis=%" PRIi32 " value=%" PRIi16,
+                    event->jaxis.axis,
+                    event->jaxis.value);
+        }
+        else if (event->type == SDL_JOYBUTTONDOWN || event->type == SDL_JOYBUTTONUP)
+        {
+            snprintf(
+                    text + size,
+                    sizeof(text) - size,
+                    " button=0x%" PRIX8, event->jbutton.button);
+        }
+    }
     SDL_FreeSurface(*surface);
     SDL_DestroyTexture(*texture);
     *surface = TTF_RenderText_Solid(font, text, black);
@@ -71,14 +120,14 @@ int main(int argc, char * const argv[])
     // The object is not used but must be opened to have controller input events
     SDL_Joystick* joystick = SDL_JoystickOpen(0);
 
-    Uint32 delay_ms = 5 * 1000; // 5 Seconds
+    Uint32 delay_ms = 10 * 1000;
     SDL_TimerID my_timer_id = SDL_AddTimer(delay_ms, exit_timer_cb, NULL);
     (void) my_timer_id;
 
     TTF_Init();
 
     //this opens a font style and sets a size
-    TTF_Font* font = TTF_OpenFont("PixelEmulator-xq08.ttf", 24);
+    TTF_Font* font = TTF_OpenFont("PixelEmulator-xq08.ttf", 12);
     if (font == NULL)
     {
         fprintf(stderr, "Failed to open font: %s\n", SDL_GetError());
@@ -112,18 +161,18 @@ int main(int argc, char * const argv[])
     // and coordinate of your texture
     SDL_RenderCopy(renderer, message, NULL, &Message_rect);
 
-    SDL_Event event;
     while (1) {
+        SDL_Event event = (SDL_Event){0};
         SDL_PollEvent(&event);
         if (event.type == SDL_QUIT)
             break;
         if (event.type == SDL_USEREVENT && (intptr_t)event.user.data1 == 10)
             break;
-        if (event.type == SDL_JOYHATMOTION && event.jhat.value == SDL_HAT_UP) {
-            bake_message(renderer, font, &surfaceMessage, &message, "Up");
-            Message_rect.w = surfaceMessage->w;
-            Message_rect.h = surfaceMessage->h;
-        }
+
+        bake_event(renderer, font, &surfaceMessage, &message, &event);
+        Message_rect.w = surfaceMessage->w;
+        Message_rect.h = surfaceMessage->h;
+
         SDL_Delay(10);
         SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
         SDL_RenderClear(renderer);
